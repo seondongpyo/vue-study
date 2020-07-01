04ebcf1e191765fc176fc74a41252274091f5955
Seondong Pyo <psd1390@gmail.com>
2020-07-01 23:34:03 +0900

    전역 컴포넌트와 지역 컴포넌트 실습

---
+++ js/app.js
@@ -0,0 +1,17 @@
+Vue.component('todo-footer', {
+    template: '<p>This is another global child component</p>'
+});
+
+var todo_list = {
+    template: '<p>This is another local child component</p>'
+};
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'This is a parent component'
+    },
+    components: {
+        'todo-list': todo_list
+    }
+});
