0f6d3bca2a498e144a7fbe9c50fcd70d0c320f65
Omnibus <psd1390@gmail.com>
2020-08-14 00:13:59 +0900

    wrapperarray 테스트

--- jest-test/src/components/__tests__/Message.test.js
+++ jest-test/src/components/__tests__/Message.test.js
@@ -3,16 +3,15 @@ import Message from '../Message';
 
 describe('Message Component Test', () => {
     test('1', () => {
-        const wrapper = shallowMount(Message, {
-            propsData: {
-                msg: 'World?'
-            }
-        });
+        const wrapper = shallowMount(Message);
+        const wrapperArray = wrapper.findAll('div > *');    // div의 모든 자식 요소
 
-        expect(wrapper.vm.msg).toBe('World?');
-        expect(wrapper.element.tagName).toBe('SPAN');
-        
-        expect(wrapper.attributes().class).toBe('msg');
-        expect(wrapper.attributes('class')).toBe('msg');
+        expect(wrapperArray.length).toBe(3);
+        expect(wrapperArray.at(0).text()).toBe('1');
+        expect(wrapperArray.at(1).text()).toBe('2');
+        expect(wrapperArray.at(2).text()).toBe('3');
+        expect(wrapperArray.at(0).element.tagName).toBe('SPAN');
+        expect(wrapperArray.at(1).element.tagName).toBe('H1');
+        expect(wrapperArray.at(2).element.tagName).toBe('P');
     });
 });
