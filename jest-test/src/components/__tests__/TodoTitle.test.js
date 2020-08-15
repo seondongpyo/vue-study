cce6b72a1cefd8b53e9a3d0a4cbd13b1ac29f9a7
Omnibus <psd1390@gmail.com>
2020-08-16 00:24:22 +0900

    spy와 Mock 모듈 추가

--- jest-test/src/components/__tests__/TodoTitle.test.js
+++ jest-test/src/components/__tests__/TodoTitle.test.js
@@ -2,6 +2,9 @@ import { shallowMount } from '@vue/test-utils';
 import TodoTitle from '../TodoTitle';
 import axios from 'axios';
 
+// Mock 모듈 생성
+jest.mock('axios');
+
 describe('TodoTitle Component Test', () => {
     let wrapper;
 
@@ -18,11 +21,18 @@ describe('TodoTitle Component Test', () => {
         //         resolve(res);
         //     });
         // });        
-        axios.get = jest.fn().mockResolvedValue(res);
+        axios.get.mockResolvedValue(res);
         wrapper = shallowMount(TodoTitle);
     });
 
     test('가져온 텍스트를 렌더링', () => {
         expect(wrapper.text()).toBe('delectus aut autem');
+    });
+
+    test('호출 여부', () => {
+        // expect(axios.get).toHaveBeenCalled();
+        const spy = jest.spyOn(wrapper.vm, 'fetchTodo');    // spyOn('스파이를 심을 메서드를 포함하는 객체', '객체에 들어있는 메서드명')
+        wrapper.vm.fetchTodo();
+        expect(spy).toHaveBeenCalled();
     });
 });
