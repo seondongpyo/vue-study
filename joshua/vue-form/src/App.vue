fc41ad9a1dc23bad3296841539c4de47a7e1e348
Omnibus <psd1390@gmail.com>
2020-08-25 00:08:38 +0900

    form 생성

--- joshua/vue-form/src/App.vue
+++ joshua/vue-form/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <div>
+        <label for="username">ID: </label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">PW: </label>
+        <input id="password" type="password">
+      </div>
+      <button>로그인</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
