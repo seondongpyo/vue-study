8c8f702e51d4ac7b60de8695d54a5a287b5852ee
Omnibus <psd1390@gmail.com>
2020-08-25 00:15:04 +0900

    form 데이터 바인딩

--- joshua/vue-form/src/App.vue
+++ joshua/vue-form/src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
-    <form>
+    <form v-on:submit="submitForm">
       <div>
         <label for="username">ID: </label>
-        <input id="username" type="text">
+        <input id="username" type="text" v-model="username">
       </div>
       <div>
         <label for="password">PW: </label>
-        <input id="password" type="password">
+        <input id="password" type="password" v-model="password">
       </div>
-      <button>로그인</button>
+      <button type="submit">로그인</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm (event) {
+      event.preventDefault();
+      console.log('form submitted');
+    }
+  }
 }
 </script>
 
