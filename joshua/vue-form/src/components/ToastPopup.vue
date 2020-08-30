2014632d23dbfcfd88154a990e7a821366ecfe39
Omnibus <psd1390@gmail.com>
2020-08-31 00:02:49 +0900

    토스트 팝업 메시지 컴포넌트 마크업 및 스타일링

--- joshua/vue-form/src/components/ToastPopup.vue
+++ joshua/vue-form/src/components/ToastPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="toast">
     토스트 팝업
   </div>
 </template>
@@ -10,6 +10,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.toast {
+    width: 200px;
+    height: 40px;
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 5%;
+    left: 5%;
+}
 </style>
