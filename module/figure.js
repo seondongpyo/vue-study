4114e8dda3e01f8340a68f45043082e9f9483d28
Omnibus <psd1390@gmail.com>
2020-09-06 00:02:35 +0900

    도형의 넓이 구하기

---
+++ module/figure.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(
+  '넓이를 구하고자 하는 도형을 입력해주세요. (정사각형, 원) : ', figure => {
+    console.log(`선택된 도형 : ${figure}`);
+
+    switch (figure) {
+      case '정사각형':
+        rl.question('한 변의 길이를 입력하세요 : ', input => {
+          console.log(`입력 받은 변의 길이 : ${input}`);
+          console.log(`정사각형의 넓이는 ${input * input} 입니다.`);
+          rl.close();
+        });
+        break;
+
+      case '원':
+        rl.question('반지름의 길이를 입력하세요 : ', input => {
+          console.log(`입력 받은 반지름의 길이 : ${input}`);
+          console.log(`원의 넓이는 ${input * input * 3.14} 입니다.`);
+          rl.close();
+        });
+        break;
+
+      default:
+        console.log('지원하지 않는 도형입니다. 정사각형 또는 원을 입력해주세요.');
+        rl.close();
+    }
+  }
+);
