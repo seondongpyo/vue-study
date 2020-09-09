a22370079fa806284ff5528c118c49e8c8bebf51
Omnibus <psd1390@gmail.com>
2020-09-10 00:01:10 +0900

    webpack 설정 파일 작성 및 번들 테스트

---
+++ module/webpack.config.js
@@ -0,0 +1,11 @@
+// __dirname, path module
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  target: 'node'
+}
